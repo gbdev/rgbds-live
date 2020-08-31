b331449876b85a4f8528994c5d3f0c36e6151803
Daid <daid303@gmail.com>
2020-08-31 22:46:56 +0200

    Add tracker

---
+++ tracker/js/ui/instruments.js
@@ -0,0 +1,113 @@
+"use strict";
+
+class InstrumentUI
+{
+    constructor()
+    {
+        this.updateInstrumentList();
+        
+        document.getElementById("instrumentSelector").onchange = (e) => { this.showSelectedInstrument() };
+    }
+
+    updateInstrumentList()
+    {
+        var select = document.getElementById("instrumentSelector");
+        while(select.options.length > 0)
+            select.options.remove(0);
+        for(var idx=0; idx<song.duty_instruments.length; idx++)
+        {
+            var i = song.duty_instruments[idx];
+            var node = document.createElement("option");
+            node.innerText = `${idx+1}: ${i.name}`;
+            node.value = idx;
+            select.children[0].appendChild(node);
+        }
+        for(var idx=0; idx<song.wave_instruments.length; idx++)
+        {
+            var i = song.wave_instruments[idx];
+            var node = document.createElement("option");
+            node.innerText = `${idx+1}: ${i.name}`;
+            node.value = idx;
+            select.children[1].appendChild(node);
+        }
+        for(var idx=0; idx<song.noise_instruments.length; idx++)
+        {
+            var i = song.noise_instruments[idx];
+            var node = document.createElement("option");
+            node.innerText = `${idx+1}: ${i.name}`;
+            node.value = idx;
+            select.children[2].appendChild(node);
+        }
+        
+        this.showSelectedInstrument();
+    }
+    
+    showSelectedInstrument()
+    {
+        var i = this.getSelectedInstrument();
+        
+        for(var row of document.getElementById("instruments").children[0].children)
+        {
+            var t = row.getAttribute("type");
+            if (t === null)
+                continue;
+            row.style.display = "";
+            if (i instanceof DutyInstrument && t.indexOf("D") > -1)
+                continue;
+            if (i instanceof WaveInstrument && t.indexOf("W") > -1)
+                continue;
+            if (i instanceof NoiseInstrument && t.indexOf("N") > -1)
+                continue;
+            row.style.display = "none";
+        }
+        
+        document.getElementById("instrumentName").value = i.name;
+
+        document.getElementById("instrumentLength").max = (i instanceof WaveInstrument) ? 255 : 63;
+        if (i.length === null)
+        {
+            document.getElementById("instrumentLengthEnabled").checked = false;
+            document.getElementById("instrumentLength").value = 0;
+        }else{
+            document.getElementById("instrumentLengthEnabled").checked = true;
+            document.getElementById("instrumentLength").value = i.length;
+        }
+        
+        if (i instanceof DutyInstrument || i instanceof NoiseInstrument)
+        {
+            document.getElementById("instrumentInitialVolume").value = i.initial_volume;
+            document.getElementById("instrumentVolumeChange").value = i.volume_sweep_change;
+        }
+        if (i instanceof DutyInstrument)
+        {
+            document.getElementById("instrumentSweepTime").selectedIndex = i.frequency_sweep_time;
+            document.getElementById("instrumentSweepChange").value = i.frequency_sweep_shift;
+
+            document.getElementById("instrumentDuty").selectedIndex = i.duty_cycle;
+        }
+        else if (i instanceof WaveInstrument)
+        {
+            document.getElementById("instrumentWaveVolume").selectedIndex = i.volume;
+            document.getElementById("instrumentWaveIndex").selectedIndex = i.wave_index;
+        }
+        else if (i instanceof NoiseInstrument)
+        {
+            document.getElementById("instrumentNoiseShiftClockMask").value = i.shift_clock_mask;
+            document.getElementById("instrumentNoiseDividingRatio").value = i.dividing_ratio;
+            document.getElementById("instrumentNoise7bit").checked = i.bit_count == 7;
+        }
+    }
+    
+    getSelectedInstrument()
+    {
+        var select = document.getElementById("instrumentSelector");
+        var option = select.selectedOptions[0];
+        if (option.parentElement.label == "Duty")
+            return song.duty_instruments[option.value];
+        if (option.parentElement.label == "Wave")
+            return song.wave_instruments[option.value];
+        if (option.parentElement.label == "Noise")
+            return song.noise_instruments[option.value];
+        return null;
+    }
+}
