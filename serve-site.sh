89026e396a02dc50032e6f1360114a9caa74fc12
Surma <surma@surma.dev>
2023-09-13 00:22:22 +0100

    Top-level build script

--- serve-site.sh
+++ serve-site.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 cd www
-
-# Generate hardware.inc.js
-echo "var hardware_inc = String.raw\`" > js/hardware.inc.js
-cat starting_project/hardware.inc | sed -r "s/\`/\$\{\'\`\'}/g" >> js/hardware.inc.js
-echo "\`;" >> js/hardware.inc.js
-
 npx http-server -c-1 
