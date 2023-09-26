f1fc5ed4d5f80d42178478ef7061a93cc6241456
Surma <surma@surma.dev>
2023-09-26 07:52:18 +0100

    Turn editors into a module

--- js/editors.js
+++ js/editors.js
@@ -1,52 +1,45 @@
 import * as storage from "./storage.js";
 
-"use strict";
+var nullEditor = {
+	hide: function () {
+		document.getElementById("nullEditorDiv").style.display = "none";
+	},
+	show: function () {
+		document.getElementById("nullEditorDiv").style.display = "";
+	},
+	setCurrentFile: function () {},
+};
+var currentEditor = nullEditor;
+var currentFilename = "";
 
-globalThis.editors = new Object();
-(function(editors) {
-    var nullEditor = {
-        'hide': function() { document.getElementById('nullEditorDiv').style.display = "none"; },
-        'show': function() { document.getElementById('nullEditorDiv').style.display = "";},
-        'setCurrentFile': function() {}
-    };
-    var currentEditor = nullEditor;
-    var currentFilename = "";
+export function getCurrentFilename() {
+	return currentFilename;
+}
 
-    editors.getCurrentFilename = function()
-    {
-        return currentFilename;
-    }
+export function setCurrentFile(filename) {
+	currentFilename = filename;
+	var prevEditor = currentEditor;
+	if (typeof storage.getFiles()[filename] == "string")
+		currentEditor = textEditor;
+	else currentEditor = gfxEditor;
+	if (prevEditor != currentEditor) {
+		prevEditor.hide();
+		currentEditor.show();
+	}
+	return currentEditor.setCurrentFile(filename);
+}
 
-    editors.setCurrentFile = function(filename)
-    {
-        currentFilename = filename;
-        var prevEditor = currentEditor;
-        if (typeof storage.getFiles()[filename] == "string")
-            currentEditor = textEditor;
-        else
-            currentEditor = gfxEditor;
-        if (prevEditor != currentEditor)
-        {
-            prevEditor.hide();
-            currentEditor.show();
-        }
-        return currentEditor.setCurrentFile(filename);
-    }
-    
-    editors.getFileType = function(filename)
-    {
-        var idx = filename.lastIndexOf(".");
-        if (idx < 0)
-            return 'binary';
-        var ext = filename.substr(idx + 1).toLowerCase();
-        if (ext == "inc") return 'text';
-        if (ext == "asm") return 'text';
-        if (ext == "z80") return 'text';
-        if (ext == "h") return 'text';
-        if (ext == "c") return 'text';
-        if (ext == "cpp") return 'text';
-        if (ext == "hpp") return 'text';
-        if (ext == "txt") return 'text';
-        return 'binary'
-    }
-})(globalThis.editors);
+export function getFileType(filename) {
+	var idx = filename.lastIndexOf(".");
+	if (idx < 0) return "binary";
+	var ext = filename.substr(idx + 1).toLowerCase();
+	if (ext == "inc") return "text";
+	if (ext == "asm") return "text";
+	if (ext == "z80") return "text";
+	if (ext == "h") return "text";
+	if (ext == "c") return "text";
+	if (ext == "cpp") return "text";
+	if (ext == "hpp") return "text";
+	if (ext == "txt") return "text";
+	return "binary";
+}
