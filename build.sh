7c4fadcedc1e49c65b168abb7072e1d63df0480c
SelvinPL <przemek.sulikowski@gmail.com>
2025-02-10 15:24:56 +0100

    Automate changing link to documentation using git tag from RGBDS

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -eu
 
 test -d binjgb/out || ./build-binjgb.sh
 test -f rgbds/rgbasm.wasm || ./build-rgbds.sh
+
+#store rgbds version tag for use in vite build
+export VITE_RGBDS_VERSION=$(git --git-dir=rgbds/.git -c safe.directory='*' describe --tags --always)
+
 npm ci
 npm run build
 
