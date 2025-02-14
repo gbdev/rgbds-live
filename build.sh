cce158b013eda749c8a440113e950c88e98ef67c
Antonio Vivace <avivace4@gmail.com>
2025-02-15 00:59:37 +0100

    apply suggestion

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ set -eu
 
 test -d binjgb/out || ./build-binjgb.sh
 test -f rgbds/rgbasm.wasm || ./build-rgbds.sh
-
-#store rgbds version tag for use in vite build
-export VITE_RGBDS_VERSION=$(git --git-dir=rgbds/.git -c safe.directory='*' describe --tags --always)
-
 npm ci
 npm run build
 
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,33 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { execSync } from 'node:child_process';
 
-export default defineConfig({
-  base: '/rgbds-live/',
-  build: {
-    target: ['chrome109', 'safari15.6', 'firefox102'],
-    outDir: 'www',
-    chunkSizeWarningLimit: 1000,
-  },
-  resolve: {
-    alias: {
-      ace: 'ace-builds/src-noconflict/',
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  process.env.VITE_RGBDS_VERSION = execSync(
+    "git --git-dir=rgbds/.git -c safe.directory='*' describe --tags --always",
+  ).toString('utf8');
+  console.log("VITE_RGBDS_VERSION set to", process.env.VITE_RGBDS_VERSION)
+  return {
+    base: '/rgbds-live/',
+    build: {
+      target: ['chrome109', 'safari15.6', 'firefox102'],
+      outDir: 'www',
+      chunkSizeWarningLimit: 1000,
     },
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: './node_modules/ace-builds/src-noconflict/**',
-          dest: 'assets/ace',
-        },
-      ],
-    }),
-  ],
+    resolve: {
+      alias: {
+        ace: 'ace-builds/src-noconflict/',
+      },
+    },
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: './node_modules/ace-builds/src-noconflict/**',
+            dest: 'assets/ace',
+          },
+        ],
+      }),
+    ],
+  };
 });
